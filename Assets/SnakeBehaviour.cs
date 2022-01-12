17ff575ceda0683ef9541eff0c30e67d5ba7905b
Ethan Sadowski <esadowski1@my.bcit.ca>
2022-01-11 17:07:38 -0800

    movement controller logging snakes

--- Assets/SnakeBehaviour.cs
+++ Assets/SnakeBehaviour.cs
@@ -2,30 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeBehaviour : MonoBehaviour
+namespace Assets
 {
-    public GameObject snakeHead;
-    private string direction = "up";
-
-    void Move()
+    public class SnakeBehaviour : MonoBehaviour
     {
-        if (this.direction == "up")
+        public GameObject snakeHead;
+        private string direction = "up";
+
+        void Move()
+        {
+            if (this.direction == "up")
+            {
+
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
         {
 
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        Debug.Log("test");
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
